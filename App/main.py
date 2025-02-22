ca2fe8331167889757978489eef2994f5f3598d5
jesuszwer <zakianovd001@gmail.com>
2025-02-22 22:10:58 +0500

    modified: main.py add asyncContextManager

--- App/main.py
+++ App/main.py
@@ -1,7 +1,20 @@
+from contextlib import asynccontextmanager
+
+from core import db_helper
 from core.config import settings
 from fastapi import FastAPI
 
-main_app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+
+    yield
+    # shutdown
+    await db_helper.dispose()
+
+
+main_app = FastAPI(lifespan=lifespan)
 
 if __name__ == "__main__":
     import uvicorn
