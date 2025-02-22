dd91b749545bcbb67afaaad2b8045b5268601832
jesuszwer <zakianovd001@gmail.com>
2025-02-22 22:21:30 +0500

    modified: configuring base model and add baseId model

--- App/core/models/base.py
+++ App/core/models/base.py
@@ -1,5 +1,19 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
+from utils import camel_case_to_snake_case
+
+from core import settings
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(naming_convention=settings.db.naming_conventions)
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{camel_case_to_snake_case(cls.__name__)}s"
+
+
+class BaseId(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
